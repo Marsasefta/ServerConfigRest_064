7db5ec4622ef44ad19d3baef2cb44aadb18d4fea
Marsasefta <marsasefta01@gmail.com>
2020-12-21 21:15:43 +0700

    Add project files.

---
+++ ServerConfigRest_064_Marsa Sefta Lesmana/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_064_Marsa_Sefta_Lesmana;
+
+namespace ServerConfigRest_064_Marsa_Sefta_Lesmana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
